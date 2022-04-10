c1b4895a6c62a5645525ba8c6dc850250b685c5c
Krzysztof Biedroń <arkendil@gmail.com>
2022-04-10 22:43:17 +0200

    Add bar plot for expected turns left fitting

--- web/src/components/turns_plot.rs
+++ web/src/components/turns_plot.rs
@@ -33,19 +33,72 @@ fn draw_plot(
             .unwrap_or(7.) as f64;
     let x_max = (words_len as f64).log2() + 1.;
 
+    let x_max_i = x_max.ceil() as i32;
+
+    let bar_per_1 = 4;
+    let mut bars = (0..(bar_per_1 * x_max_i))
+        .map(|i| {
+            (
+                (
+                    i as f64 / bar_per_1 as f64,
+                    (i + 1) as f64 / bar_per_1 as f64,
+                ),
+                Vec::<(f64, f64)>::new(),
+            )
+        })
+        .collect::<Vec<_>>();
+
+    for &(x, y, prob) in data_with_weights.iter() {
+        if let Some((_, bar_vec)) = bars.get_mut((x * bar_per_1 as f64).floor() as usize) {
+            bar_vec.push((y, prob));
+        }
+    }
+
+    let bar_data = bars
+        .into_iter()
+        .filter(|(_, bar_vec)| bar_vec.len() > 0)
+        .map(|(x, bar_vec)| {
+            let norm: f64 = bar_vec.iter().map(|&(_, prob)| prob).sum();
+            (
+                x,
+                bar_vec.into_iter().map(|(y, prob)| prob * y).sum::<f64>() / norm,
+            )
+        })
+        .collect::<Vec<_>>();
+
     root.fill(&WHITE)?;
     let root = root.margin(10u32, 10u32, 10u32, 10u32);
     let mut chart = ChartBuilder::on(&root)
         .margin(5u32)
         .x_label_area_size(30u32)
         .y_label_area_size(30u32)
-        .build_cartesian_2d(0f64..x_max, 0f64..y_max)?;
+        .build_cartesian_2d(0f64..x_max, 0f64..y_max)?
+        .set_secondary_coord(
+            (0f64..x_max)
+                .step(1. / bar_per_1 as f64)
+                .use_round()
+                .into_segmented(),
+            0f64..y_max,
+        );
 
     chart.configure_mesh().draw()?;
 
+    chart.configure_secondary_axes().draw()?;
+
+    chart.draw_secondary_series(bar_data.iter().copied().map(|((x0, x1), y)| {
+        let x0 = SegmentValue::Exact(x0);
+        let x1 = SegmentValue::Exact(x1);
+        Rectangle::new([(x0, 0.), (x1, y)], RED.mix(0.2).filled())
+    }))?;
+
+    let fit_data = bar_data
+        .into_iter()
+        .map(|((x0, x1), y)| (0.5 * (x0 + x1), y))
+        .collect::<Vec<_>>();
+
     let axis_val_multiplier = 5.;
     if data.len() >= 4 {
-        let calibration = fit(data.iter().copied().collect(), weights);
+        let calibration = fit(fit_data);
         chart
             .draw_series(LineSeries::new(
                 (0..=((axis_val_multiplier * x_max.floor()) as i32))
