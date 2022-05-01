117ea3d3102ee0326b795451ad278317e49d1615
Krzysztof Biedroń <arkendil@gmail.com>
2022-05-01 11:42:53 +0200

    Fix restart button on solver page

--- web/src/pages/solver.rs
+++ web/src/pages/solver.rs
@@ -12,11 +12,8 @@ use yew::{
     classes, function_component, html, use_effect_with_deps, use_reducer, Callback, InputEvent,
     MouseEvent, Reducible, TargetCast,
 };
-use yew_router::history::History;
-use yew_router::hooks::use_history;
 
 use crate::components::{HintedWord, SimulationDetail, ToastOption, ToastType};
-use crate::main_app::Route;
 use crate::simulation::{SimulationInput, SimulationOutput};
 use crate::util::scores_without_full_data;
 use crate::word_set::{get_current_word_set, WordSet};
@@ -106,6 +103,29 @@ struct WordState {
     error: Option<String>,
 }
 
+impl WordState {
+    pub fn initialize(word_set: &WordSet) -> WordState {
+        let (word_ind, word) = if let Some(&word) = word_set
+            .entropies
+            .as_ref()
+            .and_then(|entropies| entropies.first().map(|(word, _, _)| word))
+        {
+            (Some(word), word_set.dictionary.words[word].clone())
+        } else if let Some(word) = word_set.dictionary.words.first() {
+            (Some(0), word.clone())
+        } else {
+            (None, Word::try_from("     ").ok().unwrap())
+        };
+        WordState {
+            word_ind,
+            word,
+            hints: Hints::wrong(),
+            valid_hints: ValidHints::any(WORD_SIZE),
+            error: None,
+        }
+    }
+}
+
 impl Reducible for WordState {
     type Action = WordStateAction;
 
@@ -179,35 +199,56 @@ impl Reducible for WordState {
     }
 }
 
+enum CommonStateAction {
+    Reset(Rc<WordSet>),
+    Solver(SolverStateAction),
+    Word(WordStateAction),
+}
+
+#[derive(Clone, PartialEq)]
+struct CommonState {
+    solver_state: Rc<SolverState>,
+    word_state: Rc<WordState>,
+}
+
+impl CommonState {
+    pub fn initialize(word_set: &WordSet) -> Self {
+        Self {
+            solver_state: SolverState::default().into(),
+            word_state: WordState::initialize(word_set).into(),
+        }
+    }
+}
+
+impl Reducible for CommonState {
+    type Action = CommonStateAction;
+
+    fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
+        match action {
+            CommonStateAction::Reset(word_set) => CommonState::initialize(&*word_set).into(),
+            CommonStateAction::Solver(solver_state_action) => CommonState {
+                solver_state: self.solver_state.clone().reduce(solver_state_action),
+                word_state: self.word_state.clone(),
+            }
+            .into(),
+            CommonStateAction::Word(word_state_action) => CommonState {
+                solver_state: self.solver_state.clone(),
+                word_state: self.word_state.clone().reduce(word_state_action),
+            }
+            .into(),
+        }
+    }
+}
+
 #[function_component(Solver)]
 pub fn view() -> Html {
     let word_set = Rc::new(get_current_word_set());
-    let solver_state = use_reducer(|| SolverState::default());
     let set_toast = use_atom_setter::<ToastOption>();
-    let word_state = use_reducer(|| {
-        let (word_ind, word) = if let Some(&word) = word_set
-            .entropies
-            .as_ref()
-            .and_then(|entropies| entropies.first().map(|(word, _, _)| word))
-        {
-            (Some(word), word_set.dictionary.words[word].clone())
-        } else if let Some(word) = word_set.dictionary.words.first() {
-            (Some(0), word.clone())
-        } else {
-            (None, Word::try_from("     ").ok().unwrap())
-        };
-        WordState {
-            word_ind,
-            word,
-            hints: Hints::wrong(),
-            valid_hints: ValidHints::any(WORD_SIZE),
-            error: None,
-        }
-    });
+    let common_state = use_reducer(|| CommonState::initialize(&*word_set));
 
     let cb = {
         let set_toast = set_toast.clone();
-        let solver_state = solver_state.clone();
+        let common_state = common_state.clone();
 
         move |output: WordleWorkerOutput| match output {
             WordleWorkerOutput::SetWordSet(_name) => (),
@@ -219,14 +260,16 @@ pub fn view() -> Html {
                     scores,
                     answers,
                     knowledge,
-                } => solver_state.dispatch(SolverStateAction::NextStep {
-                    guess,
-                    hints,
-                    uncertainty,
-                    scores,
-                    answers,
-                    knowledge,
-                }),
+                } => {
+                    common_state.dispatch(CommonStateAction::Solver(SolverStateAction::NextStep {
+                        guess,
+                        hints,
+                        uncertainty,
+                        scores,
+                        answers,
+                        knowledge,
+                    }))
+                }
                 _ => set_toast(ToastOption::new(
                     "Hints missing from the step output".to_string(),
                     ToastType::Error,
@@ -257,8 +300,7 @@ pub fn view() -> Html {
     }
 
     let onclick_hints = {
-        let word_state = word_state.clone();
-        let solver_state = solver_state.clone();
+        let common_state = common_state.clone();
 
         Callback::from(move |e: MouseEvent| {
             let element: HtmlElement = e.target_unchecked_into();
@@ -267,47 +309,45 @@ pub fn view() -> Html {
                 .get("i")
                 .and_then(|i| i.parse::<usize>().ok())
             {
-                word_state.dispatch(WordStateAction::ToggleHint(
+                common_state.dispatch(CommonStateAction::Word(WordStateAction::ToggleHint(
                     i,
-                    solver_state.knowledge.clone(),
-                ));
+                    common_state.solver_state.knowledge.clone(),
+                )));
             }
         })
     };
 
     let oninput = {
-        let word_state = word_state.clone();
         let word_set = word_set.clone();
-        let solver_state = solver_state.clone();
+        let common_state = common_state.clone();
 
         Callback::from(move |e: InputEvent| {
             let input: HtmlInputElement = e.target_unchecked_into();
-            word_state.dispatch(WordStateAction::NewWord(
+            common_state.dispatch(CommonStateAction::Word(WordStateAction::NewWord(
                 input.value().clone(),
                 word_set.clone(),
-                solver_state.knowledge.clone(),
-            ));
+                common_state.solver_state.knowledge.clone(),
+            )));
         })
     };
 
     let onclick_enter = {
         let worker = worker.clone();
-        let word_state = word_state.clone();
         let set_toast = set_toast.clone();
-        let solver_state = solver_state.clone();
+        let common_state = common_state.clone();
 
         Callback::from(move |_| {
-            if let Some(guess) = word_state.word_ind {
-                if solver_state.history.len() == 0 {
+            if let Some(guess) = common_state.word_state.word_ind {
+                if common_state.solver_state.history.len() == 0 {
                     worker.send(WordleWorkerInput::Simulation(
                         SimulationInput::StartUnknownAnswer {
-                            hints: word_state.hints.to_ind(),
+                            hints: common_state.word_state.hints.to_ind(),
                             guess: Some(guess),
                         },
                     ))
                 } else {
                     worker.send(WordleWorkerInput::Simulation(SimulationInput::Continue {
-                        hints: Some(word_state.hints.to_ind()),
+                        hints: Some(common_state.word_state.hints.to_ind()),
                         guess: Some(guess),
                     }))
                 }
@@ -321,25 +361,24 @@ pub fn view() -> Html {
     };
 
     let onclick_restart = {
-        let route_history = use_history().unwrap();
-        Callback::once(move |_| {
-            route_history.push(Route::Solver);
-        })
+        let common_state = common_state.clone();
+        let word_set = word_set.clone();
+
+        Callback::once(move |_| common_state.dispatch(CommonStateAction::Reset(word_set.clone())))
     };
 
     let onclick_suggestion = {
-        let word_state = word_state.clone();
         let word_set = word_set.clone();
-        let solver_state = solver_state.clone();
+        let common_state = common_state.clone();
 
         Callback::from(move |e: MouseEvent| {
             let element: HtmlElement = e.target_unchecked_into();
             if let Some(word) = element.dataset().get("word") {
-                word_state.dispatch(WordStateAction::NewWord(
+                common_state.dispatch(CommonStateAction::Word(WordStateAction::NewWord(
                     word,
                     word_set.clone(),
-                    solver_state.knowledge.clone(),
-                ));
+                    common_state.solver_state.knowledge.clone(),
+                )));
             }
         })
     };
@@ -349,12 +388,12 @@ pub fn view() -> Html {
             <div class="container pb-2">
                 <div class="columns">
                     <div class="column col-2 col-xl-4 col-sm-6 col-xs-8 col-mx-auto text-center">
-                        <div class={classes!("form-group", word_state.error.as_ref().map(|_| "has-error"))}>
+                        <div class={classes!("form-group", common_state.word_state.error.as_ref().map(|_| "has-error"))}>
                             <label class="form-label">
                             { "Next guess" }
                             </label>
-                            <input type="text" placeholder={word_state.word.to_string()} {oninput} />
-                            if let Some(ref err) = word_state.error{
+                            <input type="text" placeholder={common_state.word_state.word.to_string()} {oninput} />
+                            if let Some(ref err) = common_state.word_state.error{
                                 <p class="form-input-hint">{ err }</p>
                             }
                         </div>
@@ -363,7 +402,7 @@ pub fn view() -> Html {
                             { "Hints (click each block to change)" }
                             </label>
                             <div onclick={onclick_hints} class="c-hand">
-                                <HintedWord word={word_state.word.clone()} hints={word_state.hints.clone()} />
+                                <HintedWord word={common_state.word_state.word.clone()} hints={common_state.word_state.hints.clone()} />
                             </div>
                         </div>
                         <button class="btn btn-primary mx-1" onclick={onclick_enter}>{ "Enter" }</button>
@@ -372,8 +411,8 @@ pub fn view() -> Html {
                 </div>
             </div>
             {{
-                let (history, init_scores) = if solver_state.history.len() > 0 {
-                    (Some(solver_state.history.clone()), None)
+                let (history, init_scores) = if common_state.solver_state.history.len() > 0 {
+                    (Some(common_state.solver_state.history.clone()), None)
                 } else if let Some(entropies) = word_set.entropies.as_ref() {
                     let init_scores = scores_without_full_data((**entropies).iter().take(10).cloned().collect::<Vec<_>>());
                     (None, Some(init_scores))
