39a7583a3382190dea2fa859ea40386749ff7386
Krzysztof Biedroń <arkendil@gmail.com>
2022-05-01 13:28:42 +0200

    Correctly resolve the number of threads available

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -12,6 +12,7 @@ mod worker;
 mod worker_atom;
 
 use gloo_worker::PublicWorker;
+use js_sys::{global, Reflect};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::{spawn_local, JsFuture};
 pub use wasm_bindgen_rayon::init_thread_pool;
@@ -23,17 +24,24 @@ pub type Dictionary = wordle_entropy_core::structs::Dictionary<WORD_SIZE>;
 pub type EntropiesData = wordle_entropy_core::structs::EntropiesData<WORD_SIZE>;
 pub type Knowledge = wordle_entropy_core::structs::knowledge::KnowledgeN<WORD_SIZE>;
 
+async fn init_threads() -> Result<JsValue, JsValue> {
+    let navigator = Reflect::get(&global(), &JsValue::from_str("navigator"))?;
+    let hardware_concurrency = Reflect::get(&navigator, &JsValue::from_str("hardwareConcurrency"))?
+        .as_f64()
+        .unwrap_or(1.) as usize;
+
+    JsFuture::from(init_thread_pool(hardware_concurrency)).await
+}
+
 #[wasm_bindgen(start)]
 pub fn start() {
-    use js_sys::{global, Reflect};
-
     // check if we are the main/UI thread
     if Reflect::has(&global(), &JsValue::from_str("window")).unwrap() {
         wasm_logger::init(wasm_logger::Config::default());
         yew::start_app::<main_app::MainApp>();
     } else {
         spawn_local(async move {
-            match JsFuture::from(init_thread_pool(12)).await {
+            match init_threads().await {
                 Ok(_) => (),
                 Err(e) => log::error!("error: {:#?}", e),
             }
